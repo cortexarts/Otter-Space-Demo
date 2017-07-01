702edd0116b2e1c2842c8c0b79c0ec1c6eec597c
JasperDre <j.drescher@cortexarts.com>
2017-07-01 17:19:02 +0200

    Intial commit with basic scene setup

---
+++ Otter Space Demo/Assets/Scripts/Gameplay/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gameManager;
+    private AudioManager audioManager;
+
+    void Start()
+    {
+        // Creating a copy of the audio manager
+        audioManager = AudioManager.instance;
+
+        if (audioManager == null)
+        {
+            Debug.LogError(this.name + ": No AudioManager found!");
+        }
+    }
+
+    void Update()
+    {
+
+    }
+
+}
---
+++ Otter Space Demo/Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager instance;
+
+    [SerializeField]
+    string hoverOverSound = "ButtonHover";
+
+    [SerializeField]
+    string pressButtonSound = "ButtonPress";
+
+    AudioManager audioManager;
+
+    private bool muted = false;
+
+    public GameObject credits;
+    private bool showCredits = false;
+
+    public GameObject pauseMenu;
+    private bool showPause = false;
+
+	public GameObject pauseButton;
+
+    public GameObject mutedBtn;
+    public GameObject UnmutedBtn;
+
+    public GameObject UpgradeUI;
+    bool upgradeEnabled = false;
+
+    void Start()
+    {
+        audioManager = AudioManager.instance;
+        if (audioManager == null)
+        {
+            Debug.LogError("No audiomanager found!");
+        }
+    }
+
+    public void StartGame()
+    {
+        audioManager.PlaySound(pressButtonSound);
+
+        if (!PlayerPrefs.HasKey("SavedLevel") || PlayerPrefs.GetInt("SavedLevel") != 1)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("SavedLevel", 1);
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+            PlayerPrefs.SetInt("SavedLevel", 2);
+            PlayerPrefs.Save();
+        }
+    }
+
+    public void QuitGame()
+    {
+        audioManager.PlaySound(pressButtonSound);
+
+        Debug.Log("WE QUIT THE GAME!");
+        Application.Quit();
+    }
+
+    public void OnMouseOver()
+    {
+        audioManager.PlaySound(hoverOverSound);
+    }
+
+    public void ToggleMute() {
+        if (muted)
+        {
+            AudioListener.volume = 0.0f;
+            UnmutedBtn.SetActive(true);
+            mutedBtn.SetActive(false);
+            muted = false;
+        }
+        else
+        {
+            AudioListener.volume = 1.0f;
+            mutedBtn.SetActive(true);
+            UnmutedBtn.SetActive(false);
+            muted = true;
+        }
+    }
+
+    public void ToggleCredits() {
+        if (showCredits)
+        {
+            credits.SetActive(false);
+            showCredits = false;
+        }
+        else
+        {
+            credits.SetActive(true);
+            showCredits = true;
+        }
+    }
+
+    public void TogglePause() {
+        if (showPause)
+        {
+            if (muted)
+            {
+                AudioListener.volume = 1.0f;
+            }
+            else
+            {
+                AudioListener.volume = 0.0f;
+            }
+            pauseMenu.SetActive(false);
+			pauseButton.SetActive(true);
+			Time.timeScale = 1;
+            showPause = false;
+        }
+        else
+        {
+            if (muted)
+            {
+                AudioListener.volume = 1.0f;
+            }
+            else
+            {
+                AudioListener.volume = 0.0f;
+            }
+            pauseMenu.SetActive(true);
+			pauseButton.SetActive(false);
+			Time.timeScale = 0;
+            showPause = true;
+        }
+    }
+
+    public void SkipScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ToggleUpgradeUI()
+    {
+        if (upgradeEnabled)
+        {
+            UpgradeUI.SetActive(true);
+            upgradeEnabled = false;
+        }
+        else
+        {
+            UpgradeUI.SetActive(false);
+            upgradeEnabled = true;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (SceneManager.GetActiveScene().name == "MainMenu")
+            {
+                Debug.Log("Quit game!");
+                Application.Quit();
+            }
+            else
+            {
+                TogglePause();
+            }
+        }
+    }
+}
