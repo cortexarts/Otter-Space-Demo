bdde93c087f99480bbd7a6fc271beff8d7df3e92
JasperDre <j.drescher@cortexarts.com>
2017-07-03 22:18:12 +0200

    Added chemistry scene and changed intro animation background

---
+++ Otter Space Demo/Assets/Scripts/UI/DescriptionManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DescriptionManager : MonoBehaviour
+{
+    public Text DescriptionTitle;
+    public Text DescriptionText;
+
+    // Use this for initialization
+    void Start ()
+    {
+        DescriptionTitle.text = "Choose one of the fuel types.";
+        DescriptionText.text = " ";
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    public void SelectedBenzine()
+    {
+        DescriptionTitle.text = "Benzine";
+        DescriptionText.text = "A propellant consisting of benzine won't come far.";
+    }
+
+    public void SelectedCoal()
+    {
+        DescriptionTitle.text = "Coal";
+        DescriptionText.text = "The only thing coal will boost is the American economy according to President Donald Trump.";
+    }
+
+    public void SelectedMethane()
+    {
+        DescriptionTitle.text = "Liquid Methane";
+        DescriptionText.text = "Liquid oxygen (LOX) and liquid methane (CH4) are cryogenic propellants. A small restartable LOX methane rocket engine can deliver 5,000 pounds-force (22 kN) thrust and a specific impulse of 321 seconds.";
+    }
+
+    public void SelectedHydrogen()
+    {
+        DescriptionTitle.text = "Liquid Hydrogen";
+        DescriptionText.text = "Liquid oxygen (LOX, O2) and liquid hydrogen (LH2, H2) are cryogenic propellants.";
+    }
+}
