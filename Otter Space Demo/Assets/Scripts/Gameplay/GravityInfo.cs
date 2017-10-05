4857c2264338f9e0c7015e86750075ea567e7c77
Stefan <stefanhulspas@gmail.com>
2017-10-05 19:17:14 +0200

    Added solid planets and a gravitational pull for these planets Bug fix: Improved movement as there was a bug in there

---
+++ Otter Space Demo/Assets/Scripts/Gameplay/GravityInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityInfo : MonoBehaviour {
+    [SerializeField]
+    private float m_GravityStrength = 1;
+
+    public float getGravityStrength()
+    {
+        return m_GravityStrength;
+    }
+}
